4a12c017381d10eb86980d92761967129bf42d8a
Aditya <sp.aditya@gmail.com>
2019-03-19 02:55:28 +0530

    Add files via upload

--- progs/next_in_series.py
+++ progs/next_in_series.py
@@ -1,17 +1,27 @@
+def validate(ip):
+    is_valid = True
+    
+    if ip.count('.') !=3:
+        print("Wrongly formatted ip address. Exiting...")
+        return False
+
+    parts = ip.split('.')
+    for part in parts:
+        if not ( 0 <= int(part) <= 255):
+            print("Not in range:", part, ". Exiting...")
+            return False
+            
+    return is_valid    
+
+        
 #1. ask the user to enter an ip address
 ip = input("Enter an ip address: ")
 
-if ip.count('.') != 3:
-    print("Wrongly formatted ip address. Exiting...")
+if not validate(ip):
     exit(-1)
+    
 
 parts = ip.split('.')
-#304, 127, 45 , 234
-for part in parts:
-    if not (int(part) >=0 and int(part) <=255):
-        print("Not in range:", part, ". Exiting...")
-        exit(-1)
-
 part_4 = int(parts[-1]) + 1
 parts[-1] = str(part_4)
 print("Next in series: ", '.'.join(parts))
