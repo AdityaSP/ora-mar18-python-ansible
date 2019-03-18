aabe2de82701342abdd919a3bfe635dffb040f63
Aditya <sp.aditya@gmail.com>
2019-03-18 23:53:42 +0530

    Update next_in_series.py

--- progs/next_in_series.py
+++ progs/next_in_series.py
@@ -1,6 +1,10 @@
 #1. ask the user to enter an ip address
 ip = input("Enter an ip address: ")
 
+if ip.count('.') != 3:
+    print("Wrongly formatted ip address.")
+    exit(-1)
+    
 #2. next in series IP address
 parts = ip.split('.')
 part_4 = int(parts[-1]) + 1
